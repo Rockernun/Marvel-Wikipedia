c568a17fa8c17b795c5b813d40308f37de98ef80
Byeong-Uk Park <114344042+Rockernun@users.noreply.github.com>
2024-06-22 23:44:33 +0900

    코드 일부 수정 & 캐릭터 이미지 추가
    비동기 호출을 더 직관적이고 간결하게 작성하기 위해 async/await 구문으로 수정했다.
    
    추가로, 각 마블 캐릭터들의 img 파일도 페이지에 띄우도록 했다.

--- src/App.js
+++ src/App.js
@@ -1,21 +1,38 @@
 import { useState, useEffect } from "react";
  
 function App() {
-  const [loading, setLoading] = useState(true);  //  로딩 state 
-  const [character, setCharacter] = useState([]);  //  item들을 끌어내서 component로 사용하기 위해 빈 배열로 character 설정
-  useEffect(() => {
-    fetch("https://marvel-proxy.nomadcoders.workers.dev/v1/public/characters?limit=50&orderBy=modified&series=24229,1058,2023")
-    .then(response => response.json())
-    .then(json => setCharacter(json.data.results));
+  const [loading, setLoading] = useState(true);  
+  const [characters, setCharacters] = useState([]);  
+  const getCharacters = async() => {
+    const response = await fetch(
+      `https://marvel-proxy.nomadcoders.workers.dev/v1/public/characters?limit=50&orderBy=modified&series=24229,1058,2023`
+    )
+    const json = await response.json();
+    setCharacters(json.data.results);
     setLoading(false);
+  }
+  useEffect(() => {
+    getCharacters();
   }, []);
   return (
     <div>
       <h1>Show the Characters!</h1>
-      {loading ? <strong>Loading...</strong> : null}
-      <ul>
-        {character.map((item) => <li key={item.id}>{item.name}</li>)}
-      </ul>
+      <hr />
+      {loading ? 
+        (<strong>Loading...</strong> 
+        ) : (
+          <div>
+            {characters.map((item) => (
+              <div key={item.id}>
+                <img src={`${item.thumbnail.path}.${item.thumbnail.extension}`} 
+                alt={item.name} 
+                />
+                <h3>{item.name}</h3>
+                <hr />
+              </div>
+            ))}
+        </div>
+        )}
     </div>
   );
 }
