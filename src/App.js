41ff4eae190293312acbbac98a418804ac99a955
Byeong-Uk Park <114344042+Rockernun@users.noreply.github.com>
2024-06-22 23:04:48 +0900

    마블 캐릭터 나열하기
    연습했던대로 useState를 사용하여 상태를 저장하고 useEffect로 최초 렌더링할 때만 API를 fetch하도록 했다.
    
    그리고 각 item들에 고유한 key값을 부여하여 구분하도록 했다.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useState, useEffect } from "react";
+ 
 function App() {
+  const [loading, setLoading] = useState(true);  //  로딩 state 
+  const [character, setCharacter] = useState([]);  //  item들을 끌어내서 component로 사용하기 위해 빈 배열로 character 설정
+  useEffect(() => {
+    fetch("https://marvel-proxy.nomadcoders.workers.dev/v1/public/characters?limit=50&orderBy=modified&series=24229,1058,2023")
+    .then(response => response.json())
+    .then(json => setCharacter(json.data.results));
+    setLoading(false);
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Show the Characters!</h1>
+      {loading ? <strong>Loading...</strong> : null}
+      <ul>
+        {character.map((item) => <li key={item.id}>{item.name}</li>)}
+      </ul>
     </div>
   );
 }
