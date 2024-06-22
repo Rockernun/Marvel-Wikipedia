7904a029f464c017a8ac170ce34eeb93b4d150db
Byeong-Uk Park <114344042+Rockernun@users.noreply.github.com>
2024-06-23 00:39:22 +0900

    routes 폴더 생성
    각각의 페이지를 분리하기 위해 routes 폴더를 생성하고 이곳에 홈 화면과 캐릭터 상세 정보 화면 파일을 만들었다.

---
+++ src/routes/Home.js
@@ -0,0 +1,39 @@
+import { useState, useEffect } from "react";
+import Characters from "../components/Characters";
+
+function Home() {
+    const [loading, setLoading] = useState(true);  
+    const [characters, setCharacters] = useState([]);  
+    const getCharacters = async() => {
+    const response = await fetch(
+      `https://marvel-proxy.nomadcoders.workers.dev/v1/public/characters?limit=50&orderBy=modified&series=24229,1058,2023`
+    )
+    const json = await response.json();
+    setCharacters(json.data.results);
+    setLoading(false);
+  }
+  useEffect(() => {
+    getCharacters();
+  }, []);
+  return (
+    <div>
+      <h1>Show the Characters!</h1>
+      <hr />
+      {loading ? 
+        (<strong>Loading...</strong> 
+        ) : (
+          <div>
+            {characters.map((item) => 
+              <Characters 
+              key={item.id}
+              thumbnail={`${item.thumbnail.path}.${item.thumbnail.extension}`} 
+              name={item.name}
+              />
+            )}
+        </div>
+        )}
+    </div>
+  );
+}
+
+export default Home;
