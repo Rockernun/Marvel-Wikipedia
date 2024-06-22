02541793b66ccf346a987fa990eec31a2bb9fa2b
Byeong-Uk Park <114344042+Rockernun@users.noreply.github.com>
2024-06-23 00:43:24 +0900

    Router 컴포넌트를 사용하여 주소에 따른 화면 전환 처리
    Routes 컴포넌트가 하는 일은 Route(꼬다리 URL)를 찾아 해당 컴포넌트를 렌더링한다.
    
    여기서 Route 컴포넌트에 직접 컴포넌트를 삽입해서 문제가 발생했는데, React Router v6에서부터는 Route 컴포넌트의 element 속성을 사용해서 렌더링할 컴포넌트를 지정해야 한다.

--- src/App.js
+++ src/App.js
@@ -1,38 +1,15 @@
-import { useState, useEffect } from "react";
-import Characters from "./Characters";
- 
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./routes/Home";
+import Detail from "./routes/Detail";
+
 function App() {
-  const [loading, setLoading] = useState(true);  
-  const [characters, setCharacters] = useState([]);  
-  const getCharacters = async() => {
-    const response = await fetch(
-      `https://marvel-proxy.nomadcoders.workers.dev/v1/public/characters?limit=50&orderBy=modified&series=24229,1058,2023`
-    )
-    const json = await response.json();
-    setCharacters(json.data.results);
-    setLoading(false);
-  }
-  useEffect(() => {
-    getCharacters();
-  }, []);
   return (
-    <div>
-      <h1>Show the Characters!</h1>
-      <hr />
-      {loading ? 
-        (<strong>Loading...</strong> 
-        ) : (
-          <div>
-            {characters.map((item) => 
-              <Characters 
-              key={item.id}
-              thumbnail={`${item.thumbnail.path}.${item.thumbnail.extension}`} 
-              name={item.name}
-              />
-            )}
-        </div>
-        )}
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/character/:id" element={<Detail />} />
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
   );
 }
 
