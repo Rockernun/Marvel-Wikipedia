6906b37a5582dd6d7964630ce9e21e0a8368dfdb
Byeong-Uk Park <114344042+Rockernun@users.noreply.github.com>
2024-06-23 13:29:42 +0900

    useParams hook 사용
    useParams hook을 사용해서 id를 받아온 후, API에 요청을 보냈다. 받아온 json 정보로 Detail 페이지에 정보를 띄우면 된다.

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,5 +1,34 @@
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+
 function Detail() {
-    return <h1>Detail</h1>;
+    const [loading, setLoading] = useState(true);
+    const [character, setCharacter] = useState([]);
+    const {id} = useParams();  //  useParams hook으로 id 받아오기
+    const getCharacters = async () => {
+        const json = await (await fetch(`https://marvel-proxy.nomadcoders.workers.dev/v1/public/characters/${id}`))
+        .json();
+        setCharacter(json.data.results);
+        setLoading(false);
+    };
+    useEffect(() => {
+        getCharacters();
+    }, []);
+    return (
+        <div>
+            <h1>Show the Character's Details...</h1>
+            {loading ? (<h1>Loading...</h1>
+        ) : (
+            <div>
+                {character.map((item) => 
+                    <Detail 
+                    
+                    />
+                )}
+            </div>
+        )}
+        </div>
+    );
 }
 
 export default Detail;
